6effbeec7a8dc4df76b8bc8535740692ad7ab4db
Charlotte Gayton <charlotte.gayton@gmail.com>
2023-11-02 14:46:32 +0000

    Created initial powershell script

---
+++ scan-sboms.ps1
@@ -0,0 +1,24 @@
+$sourceDir = 'raw'  
+$targetDir = 'raw_flatterned/'  
+
+$jsonFiles = Get-ChildItem -Path $sourceDir -Filter *.json -Recurse
+
+foreach ($file in $jsonFiles) {
+    Copy-Item -Path $file.FullName -Destination $targetDir
+}
+
+$subDirs = Get-ChildItem -Path $targetDir -Directory
+
+foreach ($dir in $subDirs) {
+    Remove-Item -Path $dir.FullName -Recurse -Force
+}
+
+$flatternedDirFiles = Get-ChildItem -Path $targetDir -Filter *.json
+
+foreach ($file in $flatternedDirFiles){
+    dotnet-covenant convert spdx $file.FullName -o $file.FullName
+}
+
+bomber scan ./raw_flatterned/ --output=json > results.json
+
+Remove-Item raw_flatterned\* -Recurse -Force
