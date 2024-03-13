0c1965852c6c956d2ddb94a2ec093ebfd91ecb78
Charlotte Gayton <charlotte.gayton@gmail.com>
2024-03-13 12:27:29 +0000

    Added backstage

--- backstage/infestigate-backstage/plugins/vulnerabilities/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { vulnerabilitiesPlugin, VulnerabilitiesPage } from './plugin';
--- backstage/infestigate-backstage/plugins/vulnerabilities/src/plugin.test.ts
+++
@@ -1,7 +0,0 @@
-import { vulnerabilitiesPlugin } from './plugin';
-
-describe('vulnerabilities', () => {
-  it('should export plugin', () => {
-    expect(vulnerabilitiesPlugin).toBeDefined();
-  });
-});
