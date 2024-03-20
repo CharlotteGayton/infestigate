0abcaf5eb8421fadbbea07be29569ade5cb9211a
Charlotte Gayton <charlotte.gayton@gmail.com>
2024-03-20 14:51:14 +0000

    Update file paths for publishing vulnerability reports to Data Lake

--- publishing_results_to_datalake/publish-results-to-datalake.ps1
+++ publishing_results_to_datalake/publish-results-to-datalake.ps1
@@ -30,8 +30,14 @@ if(!($env:DATALAKE_SASTOKEN)){
 
 $newContext = New-AzStorageContext -StorageAccountName $env:DATALAKE_NAME -SasToken $env:DATALAKE_SASTOKEN
 $filesystemName = "data"
-$fileName = "analysing_vulnerability_data/results"
+$fileName = "analysing_vulnerability_data/results/vulnerability_report.csv"
 
-$destPath = "results/"
+$destPath = "results/vulnerability_report.csv"
+
+New-AzDataLakeGen2Item -Context $newContext -FileSystem $filesystemName -Path $destPath -Source $fileName  -Force | Out-Null
+
+$fileName = "analysing_vulnerability_data/results/summary_report.csv"
+
+$destPath = "results/summary_report.csv"
 
 New-AzDataLakeGen2Item -Context $newContext -FileSystem $filesystemName -Path $destPath -Source $fileName  -Force | Out-Null
