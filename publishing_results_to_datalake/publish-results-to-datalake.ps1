52d2a6121adcf1e66bca1d9e25129b7df0d16088
Charlotte <88660116+CharlotteGayton@users.noreply.github.com>
2024-03-20 14:57:36 +0000

    Upload results to Azure cloud storage (#66)
    * Update workflow and add script for publishing results to datalake
    
    * Update Az.Storage module installation and file paths
    
    * Refactor GitHub Actions workflow
    
    * Update Az module and import Az module
    
    * Update Az module imports in run-publish-results-to-datalake.ps1
    
    * Update publish-results-to-datalake.ps1 script to install required modules and set environment variables
    
    * Update PowerShell script path in scan_sboms workflow
    
    * Update PowerShell script to include environment variables
    
    * Update PowerShell script and workflow configuration
    
    * Add newline at end of file in scan_sboms.yml
    
    * Fix file path in publish-results-to-datalake.ps1
    
    * Update file path for vulnerability report
    
    * Fix destination path in publish-results-to-datalake.ps1
    
    * Add environment variable checks for DATALAKE_NAME and DATALAKE_SASTOKEN
    
    * Update destination path for publishing results to data lake
    
    * Update destination path for publishing vulnerability report
    
    * test
    
    * Update file paths for publishing vulnerability reports to Data Lake

---
+++ publishing_results_to_datalake/publish-results-to-datalake.ps1
@@ -0,0 +1,43 @@
+$requiredModules = @(
+    @{Name="Az.Storage"; Version="5.3.0"}
+)
+$requiredModules | ForEach-Object {
+    $name = $_.Name
+    $version = $_.Version
+    if ( !(Get-Module -ListAvailable $name | ? { $_.Version -eq ($version -split "-")[0] }) ) {
+        $splat = @{
+            Name=$name
+            RequiredVersion=$version
+            Scope="CurrentUser"
+            Repository="PSGallery"
+            Force=$true
+            AllowPrerelease=$version.Contains("-")
+        }
+        Install-Module @splat
+    }
+    if (!(Get-Module $name)) {
+        # Lookup the required version of the installed module to get the path to the module manifest
+        Import-Module (Get-Module -ListAvailable $name | ? { $_.Version -eq ($version -split "-")[0] } | Select -ExpandProperty Path)
+    }
+}
+
+if(!($env:DATALAKE_NAME)){
+    Throw "DATALAKE_NAME environment variable is not set or is empty. Please set it before continuing."
+}
+if(!($env:DATALAKE_SASTOKEN)){
+    Throw "DATALAKE_SASTOKEN environment variable is not set or is empty. Please set it before continuing."
+}
+
+$newContext = New-AzStorageContext -StorageAccountName $env:DATALAKE_NAME -SasToken $env:DATALAKE_SASTOKEN
+$filesystemName = "data"
+$fileName = "analysing_vulnerability_data/results/vulnerability_report.csv"
+
+$destPath = "results/vulnerability_report.csv"
+
+New-AzDataLakeGen2Item -Context $newContext -FileSystem $filesystemName -Path $destPath -Source $fileName  -Force | Out-Null
+
+$fileName = "analysing_vulnerability_data/results/summary_report.csv"
+
+$destPath = "results/summary_report.csv"
+
+New-AzDataLakeGen2Item -Context $newContext -FileSystem $filesystemName -Path $destPath -Source $fileName  -Force | Out-Null
